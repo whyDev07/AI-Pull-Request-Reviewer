678ddbeab7bb1adb2c4a50f5e323f25ba76f2e85
Asmodeus <devadhikari5665@gmail.com>
2026-07-02 11:10:46 +0530

    Deleted unneccessary files and applied some validations

--- src/main/java/com/pr_reviewer/dto/request/ReviewRequest.java
+++ src/main/java/com/pr_reviewer/dto/request/ReviewRequest.java
@@ -1,6 +1,11 @@
 package com.pr_reviewer.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record ReviewRequest(
+        @NotBlank
+        @Pattern(regexp="[A-Za-z0-9_.-]+")
         String owner,
         String repository,
         Integer pullRequestNumber,
